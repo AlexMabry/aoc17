e5d755678834364c9a01829fbde9983c90d1872f
alex.mabry <alex.mabry@synapse-wireless.com>
2017-12-09 16:38:30 -0600

    refactor

--- day8/registers.py
+++ day8/registers.py
@@ -1,15 +1,13 @@
 from day8.day8_input import commands
 
 
+largest = 0
 registers = {}
 for c in commands:
-    condition = f'registers.get("{c[3]}", 0) {c[4]} {c[5]}'
-    if eval(condition):
-        registers[c[0]] = registers.get(c[0], 0) + c[2] if c[1] == "inc" else registers.get(c[0],0) - c[2]
+    if eval(f'registers.get("{c[3]}", 0) {c[4]} {c[5]}'):
+        registers[c[0]] = registers.get(c[0], 0) + c[2] if c[1] == "inc" else registers.get(c[0], 0) - c[2]
 
-largest = 0
 for r in registers:
-    val = registers[r]
-    largest = val if val > largest else largest
+    largest = registers[r] if registers[r] > largest else largest
 
 print(largest)
--- day8/registers_2.py
+++ day8/registers_2.py
@@ -4,9 +4,8 @@ from day8.day8_input import commands
 largest = 0
 registers = {}
 for c in commands:
-    condition = f'registers.get("{c[3]}", 0) {c[4]} {c[5]}'
-    if eval(condition):
-        registers[c[0]] = registers.get(c[0], 0) + c[2] if c[1] == "inc" else registers.get(c[0],0) - c[2]
+    if eval(f'registers.get("{c[3]}", 0) {c[4]} {c[5]}'):
+        registers[c[0]] = registers.get(c[0], 0) + c[2] if c[1] == "inc" else registers.get(c[0], 0) - c[2]
         largest = registers[c[0]] if registers[c[0]] > largest else largest
 
 print(largest)
