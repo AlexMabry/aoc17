2970830dd641e65640310129f3adf16e0063d738
alex.mabry <alex.mabry@synapse-wireless.com>
2017-12-12 09:40:39 -0600

    oops

--- day12/digital_plummer.py
+++ day12/digital_plummer.py
@@ -16,19 +16,11 @@ def add_links(old_links):
     return old_links if new_links <= old_links else add_links(old_links | new_links)
 
 
-def get_group(program):
-    group = set()
-    for p, l in programs.items():
-        if program in add_links(l):
-            group.add(p)
-    return group
-
 programs = init_programs(pipes)
-programs_in_groups = set()
-groups = 0
-for program in programs:
-    if program not in programs_in_groups:
-        groups += 1
-        programs_in_groups |= get_group(program)
 
-print(groups)
+contain_zero = set()
+for p, l in programs.items():
+    if '0' in add_links(l):
+        contain_zero.add(p)
+
+print(len(contain_zero))
