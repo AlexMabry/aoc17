f15d4e76fdec79ddb8174cf87949e3dc7ddf4912
alex.mabry <alex.mabry@synapse-wireless.com>
2017-12-13 07:57:50 -0600

    refactor

--- day13/scanner.py
+++ day13/scanner.py
@@ -7,10 +7,8 @@ for l in layers:
     firewall[int(parts[0])] = int(parts[1])
 
 severity = 0
-for step in range(max(firewall.keys())+1):
-    if step in firewall:
-        caught = step % ((firewall[step]-1)*2) == 0
-        if caught:
-            severity += step*firewall[step]
+for step, depth in firewall.items():
+    if not step % ((depth - 1) * 2):
+        severity += step * depth
 
 print(severity)
